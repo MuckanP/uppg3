140c12abf8d3cb5f54a6de27587da070231dfa2b
MuckanP <maximilian.plasberg@elev.ga.dbgy.se>
2025-09-17 10:26:13 +0200

    uppg1 load_data returnerar products

---
+++ open-test.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import locale
+
+
+products = []           #lista
+
+def format_currency(value):
+    return locale.currency(value,grouping=True)
+
+
+def load_data(filename): 
+    with open(filename, 'r') as file:       #öppnar en fil med read-rättighet
+        reader = csv.DictReader(file)
+        for row in reader:
+            id = int(row['id'])
+            name = row['name']
+            desc = row['desc']
+            price = float(row['price'])
+            quantity = int(row['quantity'])
+            
+            products.append(
+                {                   
+                    "id": id,       
+                    "name": name,
+                    "desc": desc,
+                    "price": price,
+                    "quantity": quantity
+                }
+            )
+    return products
+
+
+    
+#TODO: hur gör man så funktionen load_data returnerar products istället?
+#TODO: gör så man kan se en numrerad lista som börjar på 1.
+#TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
+#TODO: skriv en funktion som tar bort en specifik produkt med hjälp av id
+
+
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
+os.system('cls')
+
+products = load_data('db_products.csv')
+
+
+    
+
+
+
+
